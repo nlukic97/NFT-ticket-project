d3332bd4a924dc5194a52edec74dd135352069bb
Nikola <nlukic97@gmail.com>
2023-02-21 22:13:35 +0100

    added state usage to display connect wallet modal, open and close it, and display wallet connect and disconnect buttons.

--- interface/src/App.jsx
+++ interface/src/App.jsx
@@ -1,17 +1,36 @@
 import React,{useState} from 'react'
 import Nav from './components/Nav'
 import Banner from './components/Banner'
+import ConnectWalletModal from './components/connectWalletModal/ConnectWalletModal'
 
 const App = () => {
-  const [bannerClicked,setBannerClicked] = useState(false)
+  const [bannerClicked, setBannerClicked] = useState(false) // displays the banner until set to true
+  const [connectModalVisible, setConnectModalVisible] = useState(false) // hides the connect wallet modal until set to true
+  const [walletConnected, setWalletConnected] = useState(false) // when connect wallet modal is visible, will hide 'disconnect wallet' button until set to true
+  
 
-  function hideBanner(){
-    setBannerClicked(true)
+  function disconnectWallet(){
+    console.log('Disconnect wallet');
+    setWalletConnected(false)
   }
+
   return (
     <div>
-      {!bannerClicked ? <Banner hideBanner={hideBanner} /> : null}
-      <Nav />
+      {!bannerClicked ? <Banner hideBanner={()=>setBannerClicked(true)} /> : null}
+      
+      <Nav openConnectWalletModal={()=>setConnectModalVisible(true)} />
+
+      
+      {
+        (connectModalVisible) ?
+          <ConnectWalletModal 
+            closeConnectWalletModal={()=> setConnectModalVisible(false)} 
+            walletConnected={walletConnected}
+            disconnectWallet={disconnectWallet}
+          /> 
+          : null
+      }
+
     </div>
   )
 }
--- interface/src/components/Nav.jsx
+++ interface/src/components/Nav.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import Logo from './Logo'
 import "./Nav.css"
 
-const Nav = () => {
+const Nav = ({openConnectWalletModal}) => {
   return (
     <div className='navFull'>
         <nav className='navContainer'>
               <a href="./"><Logo /></a>
             <ul>
                 <li><a href="./">About</a></li>
-                <li><button>Connect Wallet</button></li>
+                <li><button onClick={openConnectWalletModal}>Connect Wallet</button></li>
             </ul>
         </nav >
     </div>
