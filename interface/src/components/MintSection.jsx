dc64384645d6490529856feee9ab013f7c5239ea
Nikola <nlukic97@gmail.com>
2023-02-25 14:49:33 +0100

    fixing up responsiveness for HeroSection, and started mint section

--- interface/src/components/MintSection.jsx
+++ interface/src/components/MintSection.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import "./MintSection.css";
 
 const MintSection = () => {
   return (
     <div>
         <div className="container ticketSpec">
           <h2>NFT ticket specifications</h2>
+
+          {/* Contains both of the tables */}
+          <div className="imgSpecContainer">
+
+            <div className="imgCard">
+              <div>
+                <img src="" alt="some img" />
+              </div>
+              <div>
+                <p>text</p>
+              </div>
+            </div>
+
+            <div className="specCard">
+              <div>e</div>
+              <div>
+                <div>one</div>
+                <div>two</div>
+              </div>
+            </div>
+
+          </div>
         </div>
     </div>
   )
