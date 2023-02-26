414ba8bf39ca64d5f1fb97d9d6781d51ea90c820
Nikola <nlukic97@gmail.com>
2023-02-26 09:14:51 +0100

    functionality to open and close modal

--- interface/src/App.jsx
+++ interface/src/App.jsx
@@ -10,34 +10,35 @@ import Modal from './components/Modal'
 const App = () => {
   const [modalOpen, setModalOpen] = useState(false)
 
-    function openModal(){
-        document.body.classList.add('modalOpen')
-        setModalOpen(true)
-      }
-      
-      function closeModal(){
-        document.body.classList.remove('modalOpen')
-        setModalOpen(false)
-    }
+  function openModal(){
+    document.body.classList.add('modalOpen')
+    setModalOpen(true)
+  }
+  
+  function closeModal(){
+    document.body.classList.remove('modalOpen')
+    setModalOpen(false)
+  }
+  
   return (
-    <div>
-      {modalOpen ? <Modal closeModal={closeModal} /> : null}
-
+    <div>      
+      {modalOpen ? <Modal closeModal={closeModal}/> : null}
+      
       <Banner />
       
       <Nav openModal={openModal}/>
-
+      
       <HeroSection/>
-
+      
       <MintSection/>
-
+      
       <footer>
         <Logo />
         <p>Terms of Service Privacy Policy</p>
         <p>&copy; 2022 Infinity Admission</p>
       </footer>
     </div>
-  )
-}
-
-export default App
+    )
+  }
+  
+  export default App
